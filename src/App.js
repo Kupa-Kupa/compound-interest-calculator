25216ca4e7a2dec8215ab44d9099aa4cf181cf32
cooper <cooper.elliott30@gmail.com>
2023-08-29 18:31:36 +1000

    Add header and update basic styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Calculator from './components/Calculator';
 import Charts from './components/Charts';
 import './App.css';
 import { dummyData } from './data/dummyData';
+import Header from './components/Header';
 
 function App() {
   const [data, setData] = useState(dummyData);
 
   return (
     <main>
+      <Header />
       <Charts data={data} />
       <Calculator setData={setData} />
     </main>
